d44822814981379653ca641569f491ca73d51e5d
ookldev <margareq@protonmail.ch>
2019-06-06 00:28:57 +0300

    Created user database schema

---
+++ server/models/Users/user.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from 'mongoose'
+
+const schema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  // TEMPORARILY SAVING PLAINTEXT PASSWORDS
+  // TODO: HASH PASSORDS WITH BCRYPT OR PBKDF2 HASHING ALGORITHM
+  password: {
+    type: String,
+    required: true,
+    trim: false
+  },
+  regDate: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+schema.set('toJSON', { virtuals: true })
+
+export default mongoose.model('User', schema)
