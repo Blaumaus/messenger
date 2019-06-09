dd49bf9864916e51dc50e1af868528b08172c99e
ookldev <margareq@protonmail.ch>
2019-06-09 22:41:17 +0300

    Written JWT wrapper

--- server/_helpers/jwt.js
+++ server/_helpers/jwt.js
@@ -1,5 +1,23 @@
 // Imports
 import { secret } from './config'
 import expressJwt from 'express-jwt'
+import { getById } from '../models/Users/user.service'
 
-// TODO: WRITE JWT VALIDATION MIDDLEWARE
+function jwt () {
+  return expressJwt({ secret, isRevoked }).unless({
+    path: [
+      // Routes which do not reqiure auth
+      '/users/register',
+      '/users/login'
+    ]
+  })
+}
+
+async function isRevoked (req, payload, done) {
+  const user = await getById(payload.sub)
+
+  // If user does not exits - revoke his token
+  if (!user) return done(null, true)
+
+  done()
+}
