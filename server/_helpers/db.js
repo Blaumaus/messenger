6714a02b17d7da2c258ea89aba6084875ce0950e
ookldev <margareq@protonmail.ch>
2019-06-09 22:41:32 +0300

    Updated database wrapper

--- server/_helpers/db.js
+++ server/_helpers/db.js
@@ -4,10 +4,13 @@ import mongoose from 'mongoose'
 import { db_creds } from './config'
 
 // Database connection
-mongoose.connect(db_creds, { useNewUrlParser: true, auth: {authdb:"admin"} })
+mongoose.connect(db_creds, { useNewUrlParser: true, auth: { authdb: 'admin' } })
     .then(db => console.log('[OK] DB is connected'))
-    .catch(err => console.log(`[ERROR] Oops. Something went wrong while connecting to the database... \n ${err}`))
+    .catch(err => console.log(`[ERROR] Oops. Something went wrong while connecting to the database => ${err}`))
 mongoose.Promise = global.Promise
 
 // Exports
-// export 
+export { default as User } from '../models/Users/user.model'
+// module.exports = {
+//     User: require('../models/Users/user.model')
+// }
