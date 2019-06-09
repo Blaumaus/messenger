3a6675d4f074e39a8e0694619a3979d36bd6aab7
kimmypracha <pracha.promtaow@gmail.com>
2019-06-09 23:42:31 +0700

    Add session section and temporarily enable using api

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import { port } from './_helpers/config'
 import express from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
+import { Session } from 'inspector';
 
 // Init app
 const app = express()
@@ -13,11 +14,17 @@ app.use(helmet({
         policy: 'no-referrer'
     }
 }))
+//Use session 
+app.use(session({
+    secret : "photonsecret",
+    resave : true , 
+    saveUninitialized : false
+}))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
-// app.use('/api', require('./routes/api/router'))
-
+app.use('/api', require('./routes/api/router'))
 app.listen(port, () => {
   console.log(`[OK] Server is running on port ${port}`)
 })
